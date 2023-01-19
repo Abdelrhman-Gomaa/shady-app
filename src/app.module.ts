59d03d5d0b5b7a6b4f0c39f3290f7cfa361ad50e
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-19 14:13:26 +0200

    feat(): Import database Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { BusketModule } from './busket/busket.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule, CourseModule, BusketModule],
   controllers: [AppController],
   providers: [AppService],
 })
