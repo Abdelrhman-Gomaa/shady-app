2b9cd271f6436512bd3c19a02e1114a8439c0ffd
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-19 17:11:35 +0200

    feat(): database module

---
+++ src/database/database.providers.ts
@@ -0,0 +1,42 @@
+import { Sequelize } from "sequelize-typescript";
+import { User } from "../user/entities/user.entity";
+import { Busket } from "../busket/entities/busket.entity";
+import { Course } from "../course/entities/course.entity";
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () =>{
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER, // 'postgres'
+        password: process.env.DATABASE_PASSWORD, //'pass123' 
+        database: process.env.DATABASE_NAME,
+        define: {
+          timestamps: false
+        }
+      });
+      sequelize.addModels([User, Busket, Course])
+      //await sequlize.sync();
+
+      // Check dataBase Connection
+      sequelize.authenticate()
+      .then(() => {
+        console.log('DataBase Connected Successfully');
+      }).catch((err) => {
+        console.log(`Oobs Database Can't Connected : ${err.message}`)
+      })
+      
+      sequelize.sync({alter: true})
+        .then(() =>{
+          console.log(`Models and relation synchronization In DB Successfully------------------------------`)
+        }).catch((err) => {
+          console.log(`Can't synchronization Models and relation In BD ------------------------------ ${err.message}`)
+        });
+      
+      return sequelize;
+    },
+  },
+];
