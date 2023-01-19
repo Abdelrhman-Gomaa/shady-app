84a3af220f82b67febe94624f4e9d53a2c26905a
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-19 17:19:44 +0200

    feat(): courses entity

---
+++ src/course/entities/course.entity.ts
@@ -0,0 +1,26 @@
+import { AutoIncrement, Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table
+export class Course extends Model{
+    
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    id: number;
+
+    @Column(DataType.STRING)
+    name: string;
+
+    @Column(DataType.STRING)
+    description: string;
+
+    @Column(DataType.STRING)
+    mediaUrl: string;
+
+    @Column(DataType.STRING)
+    resources: string;
+
+    @Column(DataType.INTEGER)
+    price: number;
+
+}
