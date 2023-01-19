bfcd3b18b96e7da3216904d23e33097127d84245
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-19 14:11:03 +0200

    feat(): swagger intialize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('El-Shady App')
+    .setDescription('This App for Student, let them to learn with love and passion')
+    .setVersion('1.0')
+    .addTag('Shady App')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('shady-app', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
