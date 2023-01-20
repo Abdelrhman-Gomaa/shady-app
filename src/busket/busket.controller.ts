a8d5cd143e5abb028c0271b304b3ace2856a7755
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-20 16:08:54 +0200

    feat(): controller methods

---
+++ src/busket/busket.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { BusketService } from './busket.service';
+import { CreateBusketDto } from './dto/create-busket.dto';
+import { UpdateBusketDto } from './dto/update-busket.dto';
+
+@ApiTags('Busket')
+@Controller('busket')
+export class BusketController {
+  constructor(private readonly busketService: BusketService) {}
+
+  @Post()
+  create(@Body() createBusketDto: CreateBusketDto) {
+    return this.busketService.create(createBusketDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.busketService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.busketService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateBusketDto: UpdateBusketDto) {
+    return this.busketService.update(+id, updateBusketDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.busketService.remove(+id);
+  }
+}
