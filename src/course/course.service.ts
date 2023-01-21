8fca8b5be03e721339865d9ba00cf8572786e22a
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-22 00:27:03 +0200

    feat(): post subject from course

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Subject } from '../subject/entities/subject.entity';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { Course } from './entities/course.entity';
@@ -8,18 +9,35 @@ export class CourseService {
   constructor(
     @Inject('COURSES_REPOSITORY')
     private readonly courseRepository: typeof Course,
+    @Inject('SUBJECTS_REPOSITORY')
+    private readonly subjectRepository: typeof Subject,
   ){}
 
   async create(createCourseDto: CreateCourseDto): Promise<Course> {
-    return await this.courseRepository.create<Course>({...createCourseDto});
+    const subject = await this.preloadSubjectByName(createCourseDto.subject);
+    createCourseDto.subject_Id = subject.id;
+
+    let result =  await this.courseRepository.create<Course>(
+      {
+        name: createCourseDto.name,
+        creator: createCourseDto.creator,
+        description: createCourseDto.description,
+        mediaUrl: createCourseDto.mediaUrl,
+        resources: createCourseDto.resources,
+        price: createCourseDto.price,
+        subject_Id: subject.id,
+        subject
+      }
+    );
+    return result;
   }
 
   async findAll(): Promise<Course[]> {
-    return await this.courseRepository.findAll();
+    return await this.courseRepository.findAll({include: {all: true}});
   }
 
   async findOne(id: number): Promise<Course> {
-    let result = await this.courseRepository.findByPk(id);;
+    let result = await this.courseRepository.findByPk(id,{include: {all: true}});;
     if(!result){
       throw new NotFoundException(`Not Found ID #${id}`);
     }
@@ -47,4 +65,12 @@ export class CourseService {
     return result;
   } 
   
+  private async preloadSubjectByName(name: string): Promise<Subject> {
+    const existingSubject = await this.subjectRepository.findOne({where :{name: name}});
+    if(existingSubject){
+        return existingSubject
+    }
+    return this.subjectRepository.create({ name });
+  }
+
 }
