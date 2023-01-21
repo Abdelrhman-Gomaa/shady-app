bd5e7c5002050670ad0b3a01ceb32ec3be3c2d04
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-22 00:26:27 +0200

    feat(): subject module for relation

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -3,13 +3,21 @@ import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { CoursesProviders } from './courses.providers';
+import { SubjectService } from 'src/subject/subject.service';
+import { SubjectModule } from 'src/subject/subject.module';
+import { SubjectsProviders } from 'src/subject/subject.providers';
 
 @Module({
-  imports:[DatabaseModule],
+  imports:[
+    DatabaseModule,
+    SubjectModule
+  ],
   controllers: [CourseController],
   providers: [
     CourseService,
-    ...CoursesProviders
+    ...CoursesProviders,
+    SubjectService,
+    ...SubjectsProviders
   ]
 })
 export class CourseModule {}
